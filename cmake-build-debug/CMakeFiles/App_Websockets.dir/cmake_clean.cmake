3afb9333b52049c4b4de15cb10ab2535ecdb73aa
Arthur-Kamau <arthur-kamau@pop-os.localdomain>
2020-08-31 11:31:22 +0300

    project scaffold

---
+++ cmake-build-debug/CMakeFiles/App_Websockets.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "App_Websockets"
+  "App_Websockets.pdb"
+  "CMakeFiles/App_Websockets.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/App_Websockets.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
