56b8c01f6f3a3dc52ba249d6d963cae3f25a0dc0
Arthur-Kamau <kennkamau09@gmail.com>
2020-12-10 18:47:33 +0300

    move phx project up, edit connection props

--- js_test_client/client.js
+++ js_test_client/client.js
@@ -1,18 +1,20 @@
-const { Socket } = require('phoenix-channels')
+
  
 
 
 
 (function client() {
 
-    let socket = new Socket("ws://example.com/socket")
+  const { Socket } = require('phoenix-channels')
+    let socket = new Socket("ws://localhost:4000/socket")
  
     socket.connect()
      
     // Now that you are connected, you can join channels with a topic:
-    let channel = socket.channel("room:lobby", {})
+    let channel = socket.channel("test:lobby", {})
     channel.join()
       .receive("ok", resp => { console.log("Joined successfully", resp) })
+      .receive("join", resp => { console.log("Somenone Joined successfully", resp) })
       .receive("error", resp => { console.log("Unable to join", resp) })
      
 })();
